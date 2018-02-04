ea6c15fb94ed332976206419a9535919f4dc7639
Nicolas Mauthes <nmauthes@netscape.net>
2018-02-03 21:09:39 -0800

    first commit

---
+++ src/UIDNetworkDriver.java
@@ -0,0 +1,23 @@
+public class UIDNetworkDriver {
+
+    public static void main(String[] args) {
+        // TODO implement network logic
+    }
+
+}
+
+class MockComputer { // TODO implement computer methods
+
+    MockComputer() {
+
+    }
+
+    int round(int i) {
+        return 0;
+    }
+
+    int getId() {
+        return 0;
+    }
+
+}
